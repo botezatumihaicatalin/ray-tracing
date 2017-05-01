8798a1f8ef9e7f93ed0ca83de7f89c67f3d16f33
botezatumihaicatalin <botezatu.mihaicatalin@gmail.com>
2017-05-01 09:39:41 +0300

    Cleaning up: - Removing hardcoded glm/cimg files (adding them as submodules). - Moving source code into src/ directory. - Making visual studio project include /vendor folder

---
+++ src/kernel.cu
@@ -0,0 +1,59 @@
+#define GLM_FORCE_CUDA
+#define CUDA_VERSION 8000
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <memory>
+#include <ctime>
+
+#include "rendering/Scene.h"
+#include "cimg/CImg.h"
+
+int main() {
+  Scene scene(800, 600);
+  scene.antialiasing(false);
+  cimg_library::CImg<float> image(scene.width(), scene.height(), 1, 3, 0);
+
+  cimg_library::CImgDisplay main_disp(image, "Click a point");
+  while (!main_disp.is_closed()) {
+    
+    
+    clock_t t0 = clock();
+    std::unique_ptr<glm::vec3[]> pixels(scene.render());
+
+    uint32_t i = 0;
+    for (size_t y = 0; y < scene.height(); y++) {
+      for (size_t x = 0; x < scene.width(); x++, i++) {
+        for (size_t c = 0; c < 3; c++) {
+          image(x, y, 0, c) = pixels[i][c];
+        }
+      }
+    }
+    clock_t t1 = clock();
+    printf("Render = %f secs\n", float(t1 - t0) / 1000);
+
+    image.display(main_disp);
+
+    if (main_disp.is_keyW()) {
+      scene.camera().move_forward(0.3f);
+    }
+
+    if (main_disp.is_keyS()) {
+      scene.camera().move_backward(0.3f);
+    }
+
+    if (main_disp.is_keyR()) {
+      scene.camera().rotate(-0.1f);
+    }
+
+    if (main_disp.is_keyE()) {
+      scene.camera().rotate(0.1f);
+    }
+
+    main_disp.wait();
+  }
+
+  return 0;
+}
---
+++ src/rendering/Ray.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "glm/vec3.hpp"
+
+class Ray {
+
+private:
+  glm::vec3 origin_;
+  glm::vec3 direction_;
+
+public:
+  __host__ __device__ Ray(const glm::vec3& origin, const glm::vec3& direction)
+    : origin_(origin), direction_(glm::normalize(direction)) {}
+
+  __host__ __device__ const glm::vec3& direction() const;
+  __host__ __device__ const glm::vec3& origin() const;
+};
+
+inline const glm::vec3& Ray::direction() const {
+  return direction_;
+}
+
+inline const glm::vec3& Ray::origin() const {
+  return origin_;
+}
+
+
+
